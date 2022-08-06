5ef0f1ce4f0ba465cab3b2180a094c192c354b0a
unknown <taco.sauce.dev@gmail.com>
2022-08-05 20:09:00 -0600

    NetworkChanges.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,19 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-
+using TMPro;
+using System.Linq;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using Unity.Netcode.Transports.UTP;
+using Unity.Netcode;
 public class SceneLoader : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI _joinCode; // Join Code the player types in
+    [SerializeField] private TextMeshProUGUI _lobbyCode; // Lobby Code that the player gives for others to enter into join code field.
+    [SerializeField] private GameObject _createGamePanel;
+
+    public async void JoinGame(){
+        Debug.Log("JoinCode: " + _joinCode.text);
+        try
+        {   
+            
+            await MatchmakingService.JoinLobbyWithAllocation(_joinCode.text);
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        } 
+    }
+    public async void CreateGame(){
+        
+        await MatchmakingService.CreateLobbyWithAllocation();
+        NetworkManager.Singleton.StartHost();
+        
+        _lobbyCode.SetText(MatchmakingService.GetLobbyId());
+
+        _createGamePanel.SetActive(true);
+        
+
+    }   
     public void ExitGame(){
         Application.Quit();
         Debug.Log("Quitting!");
     }
 
-    public void NewGame(){
+    public async void NewGame(){
         Debug.Log("Loading Scene!");
-        SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+        await Authentication.Login();
+        
+        Debug.Log(Authentication.PlayerId);
+        SceneManager.LoadSceneAsync("Lobby", LoadSceneMode.Single);
     }
 
     public void Options(){
